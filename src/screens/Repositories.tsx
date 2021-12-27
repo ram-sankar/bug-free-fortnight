6bd589695390df308f3ba911f47bb35d8703df76
Ram Sankar <mailtoramsankar@gmail.com>
2021-12-27 17:28:53 +0530

    Completed Repository Screen

--- src/screens/Repositories.tsx
+++ src/screens/Repositories.tsx
@@ -1,44 +1,116 @@
-import React, { useContext } from "react";
-import { View, StyleSheet, Text, Image } from "react-native";
-import { NavigationContext } from '@react-navigation/native';
+import { AxiosError } from "axios";
+import React, { useContext, useEffect, useState } from "react";
+import { View, StyleSheet, Text, FlatList } from "react-native";
+import repoServices from "../api/repoServices";
 
 import AuthContext from "../auth/context"
-import AppButton from "../components/Button";
+import { colors, sizes } from "../common/theme";
 
 function Repositories() {
   const { user } = useContext(AuthContext);
+  const [repos, setRepos] = useState([]);
+
+  useEffect(() => {
+    getRepos()
+  },[]);
+  
+  const getRepos = async () => {
+    try {
+      const response: any = await repoServices.getRepos();
+      setRepos(response?.data);
+    } catch(e) {
+      const error = e as AxiosError;
+      console.log(error.message);
+    }
+  }
+
+  const RenderEmptyMessage = () => (
+    <View>
+      <Text style={styles.emptyMessage}>Your account has no repositories</Text>
+    </View>
+  )
+
+  const Header = () => (
+    <Text style={styles.heading}>Your Repositories</Text>
+  )
+
+  const RenderList = ({item}: any) => (
+    <View style={styles.repoDetails}>
+      <View style={styles.repoHeading}>
+        <Text style={styles.repoName}>{item.name}</Text>
+        <Text style={styles.repoVisibility}>{item.visibility}</Text>
+      </View>
+      <Text style={styles.repoDate}>{new Date(item.created_at).toISOString().split('T')[0]}</Text>
+      <Text style={styles.repoDescription}>{item.description}</Text>
+    </View>
+  )
+
+  const RenderRepos = () => (
+    <View style={{flex: 1}}>
+      <Header />
+      <FlatList
+        data={repos}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={RenderList}
+        showsVerticalScrollIndicator={true}
+      />
+    </View>
+  )
+
   return (
       <View style={styles.container}>
-        <Text style={styles.userName}>{user?.name}</Text>
-        <Text style={styles.email}>{user?.email}</Text>
+        {repos.length!==0 ? <RenderRepos/> : <RenderEmptyMessage/>}
       </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center'
+    flex: 1
   },
-  image: {
-    width: 200,
-    height: 200,
-    marginTop: 50
-  },
-  userName: {
-    marginTop: 20,
+  emptyMessage: {
+    textAlign: 'center',
+    marginTop: 60,
     fontWeight: '700',
-    fontSize: 22
+    fontSize: sizes.fontXL
   },
-  email: {
-    fontSize: 16,
-    marginBottom: 20,
+  heading: {
+    textAlign: 'center',
+    marginTop: 60,
+    fontWeight: '700',
+    fontSize: sizes.fontXL,
+    color: colors.darkGrey
   },
-  buttonContainer: {
+  repoHeading: {
     flexDirection: 'row',
+    width: '100%'
   },
-  logoutButton: {
-    width: 150,
-    marginHorizontal: 10
+  repoName: {
+    flex: 1,
+    fontWeight: '700',
+    fontSize: sizes.fontL,
+    color: colors.primary,
+  },
+  repoDescription: {
+    fontSize: sizes.font,
+    color: colors.darkGrey,
+  },
+  repoDate: {
+    fontSize: sizes.fontS,
+    color: colors.darkGrey,
+  },
+  repoDetails: {
+    marginTop: 10,
+    marginHorizontal: 20,
+    borderRadius: 10,
+    backgroundColor: colors.white,
+    paddingVertical: 10,
+    paddingHorizontal: 20
+  },
+  repoVisibility: {
+    fontWeight: '700',
+    fontSize: sizes.fontS,
+    color: colors.darkGrey,
   }
 });
 export default Repositories;
