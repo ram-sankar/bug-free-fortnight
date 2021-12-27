f9b1a1b6d4877704086b40190ee34a09ca5b3423
Ram Sankar <mailtoramsankar@gmail.com>
2021-12-27 08:33:30 +0530

    Store access_token at Secure Store

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -3,6 +3,10 @@ import * as WebBrowser from 'expo-web-browser';
 import { useAuthRequest } from 'expo-auth-session';
 import { Button } from 'react-native';
 import Config from "react-native-config";
+import Axios from 'axios';
+
+import Auth from "../auth/storage"
+import { useEffect } from 'react';
 
 WebBrowser.maybeCompleteAuthSession();
 
@@ -14,6 +18,18 @@ const discovery = {
 
 
 export default function Home2() {
+  useEffect(()=>{
+    fun();
+  })
+  const fun = async () => {
+    Axios.defaults.headers.common["Authorization"] = await Auth.getToken() || '';
+    try {
+      const res = await Axios.get('https://api.github.com/user');
+      console.log('a', res.data);
+    } catch (e: any) {
+      console.log(e.response);
+    }
+  }
   const [request, response, promptAsync] = useAuthRequest(
     {
       clientId: process.env.CLIENT_ID || '',
@@ -24,10 +40,31 @@ export default function Home2() {
     discovery
   );
 
+  const getAccessToken = async (code: string) => {
+    const response = await Axios.post(discovery.tokenEndpoint, {},
+      {
+        params: {
+          client_id: process.env.CLIENT_ID,
+          client_secret: process.env.CLIENT_SECRET,
+          code: code
+        },
+        headers: {
+          'content-type': 'text/json',
+          'Accept': 'application/json'
+        }
+      })
+    console.log(response.data);
+    if (response.data?.access_token) {
+      await Auth.storeToken(response.data?.token_type + ' ' + response.data?.access_token);
+      fun();
+    }
+    
+  }
+
   React.useEffect(() => {
     if (response?.type === 'success') {
       const { code } = response.params;
-      console.log(code);
+      getAccessToken(code);
     }
   }, [response]);
 
